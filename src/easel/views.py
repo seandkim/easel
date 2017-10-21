9c8f186af769463293ce318557b51e60b9730989
Tiffany Lee <lyeukyu@gmail.com>
2017-10-21 00:19:12 -0400

    added base template and static files:'

--- src/easel/views.py
+++ src/easel/views.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+# bunch of imports we probably need later on
+from datetime import datetime
+from django.core.mail import EmailMessage
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout, tokens
+from django.db import transaction
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template import Context
+from django.template.loader import get_template
+from django.urls import reverse
+from django.utils.dateparse import parse_datetime
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
+from time import localtime, strftime
 
 # Create your views here.
+def test_view(request):
+	return render(request, 'base.html', {})
--- src/webapps/urls.py
+++ src/webapps/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from easel import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^testpage/', views.test_view, name="test_view"),
 ]
