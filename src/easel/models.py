9f577f80d0bb69266b455a210d80faeaa976b5ee
HyunJung Kim <hyunjungkim@HyunJungs-MBP.wv.cc.cmu.edu>
2017-10-21 20:23:20 -0400

    User and Profile added

--- src/easel/models.py
+++ src/easel/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(max_length=40)
+    firstname = models.CharField(max_length=40)
+    lastname = models.CharField(max_length=40)
+    confirmlink = models.CharField(max_length=200, default="")
+    confirmed = models.BooleanField(default=0)
+  
+    def __unicode__(self):
+        return self.fullname
