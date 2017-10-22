c982d06fa16922934a5b12f47edf68535fa6ca3a
HyunJung Kim <hyunjungkim@HyunJungs-MBP.wv.cc.cmu.edu>
2017-10-22 16:05:02 -0400

    models completed & abstract user deleted

--- src/easel/models.py
+++ src/easel/models.py
@@ -2,20 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class EaselUser(AbstractUser):
-    pass
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-<<<<<<< HEAD
     user = models.OneToOneField(User)
-=======
-    user = models.OneToOneField(EaselUser)
     username = models.CharField(max_length=40)
->>>>>>> 546b83f0466afc9e508d34734d7d279b94213923
     firstname = models.CharField(max_length=40)
     lastname = models.CharField(max_length=40)
     # age = models.IntegerField(default=20, blank=True)
@@ -25,3 +17,66 @@ class Profile(models.Model):
 
     def __unicode__(self):
         return self.username
+
+    
+class Project(models.Model):
+    owner = models.ForeignKey(Profile)
+    name = models.CharField(max_length=20)
+    date = models.DateField(max_length=20)
+    description = models.CharField(max_length=1000)
+    
+    def __unicode__(self):
+        return self.name
+
+
+class Media(models.Model):
+    owner = models.ForeignKey(Profile)
+    name = models.CharField(max_length=20)
+    project = models.ForeignKey(Project)
+    mediatype = models.CharField(max_length=10)
+#    mediatype = (
+#        (IMAGE, 'image'),
+#        (AUDIO, 'audio'),
+#        (VIDEO, 'video'),
+#    )
+    file = models.FileField(upload_to='')
+    caption = models.CharField(max_lenth=200)
+    
+    def __unicode__(self):
+        return self.name
+    
+    
+class Site(models.Model):
+    owner = models.ForeignKey(Profile)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=1000)
+    url = models.CharField()
+    
+    def __unicode__(self):
+        return self.name
+
+    
+class Page(models.Model):
+    owner = models.ForeignKey(Profile)
+    name = models.CharField(max_length=40)
+    site = models.ForeignKey(Site)
+    nrow = models.IntegerField()
+    ncol = models.IntegerField()
+    url = models.CharField()
+    
+    def __unicode__(self):
+        return self.name
+    
+
+class Component(models.Model):
+    owner = models.ForeignKey(Profile)
+    name = models.CharField(max_length=40)
+    page = models.ForeignKey(Page)
+    html = models.CharField()
+    nrow = models.IntegerField()
+    ncol = models.IntegerField()
+    rowIndex = models.IntegerField()
+    colIndex = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
--- src/webapps/settings.py
+++ src/webapps/settings.py
@@ -95,7 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-AUTH_USER_MODEL = 'easel.EaselUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
