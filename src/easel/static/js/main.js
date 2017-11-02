1ff45a052ae8c751343ac70ef4ff853382fc3613
Tiffany Lee <lyeukyu@gmail.com>
2017-11-01 23:25:47 -0400

    added sidebar sliding animation

--- src/easel/static/js/main.js
+++ src/easel/static/js/main.js
@@ -1,15 +1,29 @@
-/* sidebar javascript */
 $(function() {
-	var settings = {
-      		toggle: ".sliiider-toggle",
-      		exit_selector: ".slider-exit",
-      		animation_duration: "0.5s",
-     		place: "right", //where is the menu sliding from, possible options are (left | right | top | bottom)
-      		animation_curve: "cubic-bezier(0.54, 0.01, 0.57, 1.03)", //animation curve for the sliding animation
-      		body_slide: true, //set it to true if you want to use the effect where the entire page slides and not just the div
-     		no_scroll: false, //set to true if you want the scrolling disabled while the menu is active
-            auto_close: false //set to true if you want the slider to auto close everytime a child link of it is clicked
-	};
+	/* variable declarations */
+    var sidebarHidden = false;
 
-	var menu = $('#menu').sliiide(settings);
+    // hide sidebar
+    $('#sidebar').css('right', '-' + $('#sidebar').outerWidth() + 'px');
+    
+    /* event listeners */
+    $('#toggle').click(sidebarToggle);
+    $('#slider-exit').click(sidebarToggle);
+
+    /* 
+     * Controls hide and show of sidebar
+     */
+    function sidebarToggle() {
+        if (sidebarHidden) {
+            $("#sidebar").animate({ right: 0 }, {
+                duration: 1000,
+                easing: 'easeOutQuart'
+            });
+        } else {
+            $("#sidebar").animate({ right: '-' + $('#sidebar').outerWidth() + 'px' }, {
+                duration: 1000,
+                easing: 'easeOutQuart'
+            });
+        }
+        sidebarHidden = !sidebarHidden;
+    }
 });
