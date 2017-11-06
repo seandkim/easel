e5308ec5e5b9ab2f4cf7eacbf9eb861022300857
Sean Kim <seandkim14@gmail.com>
2017-11-05 22:23:25 -0500

    typing on site editor works

---
+++ src/easel/static/js/site-editor-form.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  // Periodically refresh stream page
+  // CSRF set-up copied from Django docs
+  // from code provided during class
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+
+  var csrftoken = getCookie('csrftoken');
+  $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+      xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    }
+  });
+
+  $(document).keypress(typeOnForm)
+
+  function typeOnForm(event) {
+    console.log(event.which, c)
+    var c = String.fromCharCode(event.which)
+    $('#cursor').prev().append(c)
+  }
+});
