1f6743ded1b79573b02f18c08b714979ad83553d
Tiffany Lee <lyeukyu@gmail.com>
2017-11-16 18:20:29 -0500

    added editor_ajax.js for ajax calls

---
+++ src/easel/static/js/editor_ajax.js
@@ -0,0 +1,64 @@
+'use strict';
+
+$(function() {
+    /* ajax set up */
+    // set up csrf tokens
+    // https://docs.djangoproject.com/en/1.10/ref/csrf/
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    var csrftoken = getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+   
+    /* make ajax call to page actions */
+    $( "#publish" ).click(function() {
+      console.log("sent ajax request to publish");
+      $.ajax({
+          url: "/sites/dummy/publish/",
+          method: "POST",
+          data: { html : $('#page-content').html() },
+          success: function(data) {
+            console.log("successful sent html of page");
+          }
+      });
+    });
+
+    /* make ajax call to page actions */
+    $( ".new-page" ).click(function() {
+      console.log("sent ajax request to add page");
+      $.ajax({
+          url: "^sites/dummy/editor/addPage/",
+          method: "POST",
+          data: { html : $('#page-content').html() },
+          success: function(data) {
+            console.log("successful sent html of page");
+          }
+      });
+    });
+
+});
--- src/easel/static/js/main.js
+++ src/easel/static/js/main.js
@@ -1,39 +1,6 @@
 'use strict';
+
 $(function() {
-    /* ajax set up */
-    // set up csrf tokens
-    // https://docs.djangoproject.com/en/1.10/ref/csrf/
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-
-    var csrftoken = getCookie('csrftoken');
-
-    function csrfSafeMethod(method) {
-        // these HTTP methods do not require CSRF protection
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
-
 	  /* variable declarations */
     var sidebarHidden = true;
 
@@ -134,19 +101,6 @@ $(function() {
 
     var editor = new MediumEditor('.editable');
 
-   
-    /* make ajax call to page actions */
-    $( "#publish" ).click(function() {
-      console.log("sent ajax request to update");
-      $.ajax({
-          url: "/easel/savePage",
-          method: "POST",
-          data: { html : $('#page-preview').html() }
-      }).success(function(data) {
-          console.log("successful sent html of page");
-      });
-    });
-
     var cr_tabs = $('.cr-tabs > li');
     cr_tabs.on("click", function() {
        // get the unactivated tab
@@ -194,36 +148,6 @@ $(function() {
     }, function() {
        $(this).find('a').html('<i class="icon-plus"></i>');
     });
-
-    /* create graphs in dashboard */
-    // var trace1 = {
-    //   x: ["Mon", "Tue", "Wed", "Thur", "Fri"],
-    //   y: [1, 6, 3, 6, 1],
-    //   mode: 'markers',
-    //   type: 'scatter',
-    //   name: 'Site A',
-    //   marker: { size: 12 }
-    // };
-
-    // var trace2 = {
-    //   x: ["Mon", "Tue", "Wed", "Thur", "Fri"],
-    //   y: [4, 1, 7, 1, 4],
-    //   mode: 'markers',
-    //   type: 'scatter',
-    //   name: 'Site B',
-    //   marker: { size: 12 }
-    // };
-
-    // var data = [ trace1, trace2 ];
-
-    // var layout = {
-    //   yaxis: {
-    //     range: [0, 8]
-    //   },
-    //   title:'Visitors from last Week'
-    // };
-
-    // Plotly.newPlot('graph1', data, layout);
 
 
 });
