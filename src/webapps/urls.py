5222dbc8723ce2fd54413ca44f63d9c6acc2ab59
Stella Kim <hyunjunk@andrew.cmu.edu>
2017-11-28 18:52:36 -0500

    implemented custom page 404 - complete

--- src/webapps/urls.py
+++ src/webapps/urls.py
@@ -25,8 +25,6 @@ import views_home
 import easel.views
 
 
-handler404 = 'easel.views.views_error.pageNotFound'
-
 urlpatterns = [
 #    url(r'^$', easel.views.views_home.home),
     url(r'^$', views_home.home),
